538c163f609b8b30d3d2bb2630bfa056104039c3
Kaif-2804 <77483504+Kaif-2804@users.noreply.github.com>
2021-04-15 13:52:10 +0200

    entered newline to get rid of extra line

---
+++ CSVwrite.py
@@ -0,0 +1,7 @@
+import csv
+
+with open('newCSV.csv', mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    csv_writer.writerow(['John Smith', 'Accounting', 'November'])
+    csv_writer.writerow(['Erica Meyers', 'IT', 'March'])
